79785fb8f2a07309ed00043bd4a6e309b1544073
dluisvaldivia <dluis.valdivia@gmail.com>
2026-05-21 22:14:20 +0200

    Set BrowserRouter basename to BASE_URL for GitHub Pages subpath

--- src/App.tsx
+++ src/App.tsx
@@ -31,7 +31,7 @@ const ContactPlaceholder = () => (
 
 const App: React.FC = () => {
   return (
-    <Router>
+    <Router basename={import.meta.env.BASE_URL}>
       <Wrapper>
         <Routes>
           <Route path="/" element={<SoDivergent />} />
